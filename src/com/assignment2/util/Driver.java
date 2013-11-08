1993f8a7f3e432f3f8fb836b4941b7f3ebcfb81a
Santiago <sfinoli@yahoo.com>
2013-11-07 22:45:42 -0500

    sql for db
    Line 25 of Driver in util package. You can add your sql statements for
    the server in the sql variable. Then just run the driver.

--- src/com/assignment2/util/Driver.java
+++ src/com/assignment2/util/Driver.java
@@ -1,16 +1,30 @@
 package com.assignment2.util;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class Driver {
-
-	public static void main(String[] args) 
+public class Driver 
+{
+	public static void main(String[] args) throws SQLException 
 	{
 		Connection connection = DatabaseConnection.getConnection();
 
 		if (connection != null) 
 		{
 			System.out.println("Connection : true");
+			
+			Statement statement = null;
+			try 
+			{
+				statement = connection.createStatement();
+			} catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+			String sql = "";
+			System.out.println(statement.execute(sql));
+			
 			DatabaseConnection.close(null, null, connection);
 		} 
 		else 
